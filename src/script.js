2dd6110ad4d627bce012ad365db569c82e82532a
Srijan Rawat <srijanrawat3@gmail.com>
2026-05-14 00:03:36 +0530

    UI: initial UI

--- src/script.js
+++ src/script.js
@@ -1,13 +1,13 @@
-async function getUser(){
-    try{
-        const res = await fetch(`https://api.github.com/users/srijanstack`);
-        const data = await res.json();
-        console.log(data)
-    }catch{
+// async function getUser(){
+//     try{
+//         const res = await fetch(`https://api.github.com/users/srijanstack`);
+//         const data = await res.json();
+//         console.log(data)
+//     }catch{
 
-    }finally{
+//     }finally{
         
-    }
-}
+//     }
+// }
 
-getUser();
+// getUser();
